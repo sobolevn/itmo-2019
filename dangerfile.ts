c600784d2922de94dc4392dd0bf7ae691af94568
sobolevn <mail@sobolevn.me>
2019-09-14 13:54:09 +0300

    Adds danger

---
+++ dangerfile.ts
@@ -0,0 +1,10 @@
+import { fail, danger } from 'danger'
+
+// Critical:
+if (danger.github.base.ref !== 'master') {
+  fail('We only accept PRs to `master` branch.')
+}
+
+if (danger.github.base.head.ref.match(/homework-\d+/)) {
+  fail('Your branch should be named `homework-${HOMEWORK_NUMBER}`.')
+}
