b0323a48b0d057eebafe821598e4b903bbaa036c
sobolevn <mail@sobolevn.me>
2019-09-14 14:12:37 +0300

    Removes rebase action

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,14 @@
-import { fail, danger } from 'danger'
+import { message, fail, danger } from 'danger'
 
 // Critical:
+console.log(JSON.stringify(danger.github))
+console.log(JSON.stringify(danger.github.base))
+console.log(JSON.stringify(danger.github.head))
+
+message(JSON.stringify(danger.github))
+message(JSON.stringify(danger.github.base))
+message(JSON.stringify(danger.github.head))
+
 if (danger.github.base.ref !== 'master') {
   fail('We only accept PRs to `master` branch.')
 }
