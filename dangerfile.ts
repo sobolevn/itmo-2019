12d63d780c24dd9d4048b6ad5e1f83c8c9ab6eca
sobolevn <mail@sobolevn.me>
2019-09-14 13:58:16 +0300

    Fixes danger ci

--- dangerfile.ts
+++ dangerfile.ts
@@ -5,6 +5,6 @@ if (danger.github.base.ref !== 'master') {
   fail('We only accept PRs to `master` branch.')
 }
 
-if (danger.github.base.head.ref.match(/homework-\d+/)) {
+if (danger.github.head.ref.match(/homework-\d+/)) {
   fail('Your branch should be named `homework-${HOMEWORK_NUMBER}`.')
 }
