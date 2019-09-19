3506a7a5deeeffed867558a2f6144aab875be6d1
sobolevn <mail@sobolevn.me>
2019-09-19 23:01:56 +0300

    Improves CI

--- bin/homework1.sh
+++ bin/homework1.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -f 'students/runtests.py' ]; then
+  echo "runtests.py exist in the root students/ folder!"
+  exit 1
+fi
+
 for homework_folder1 in $(find students/* -maxdepth 1 -name 1); do
   if [ ! -f "$homework_folder1/runtests.py" ]; then
     echo "runtests.py does not exist in $homework_folder1"
